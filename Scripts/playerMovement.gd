2f086a80ae89fe78b4e685cc7d0ff1da7f395465
Marcos Mundina Fabregat <discor2008125@gmail.com>
2025-11-05 18:45:00 +0100

    Replace `DirtTileSet` with `TileSet` for enhanced tilemap functionality
    Includes:
    - New `TileSet.tres` with integrated physics properties.
    - Removal of `DirtTileSet.tres`.
    - Updated `level1.tscn` with additional layers and background integration.
    - Adjusted `playerMovement.gd` for slower movement and jump velocity.

--- Scripts/playerMovement.gd
+++ Scripts/playerMovement.gd
@@ -1,8 +1,8 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+const SPEED:float = 150.0
+const JUMP_VELOCITY:float = -200.0
 
 
 func _physics_process(delta: float) -> void:
