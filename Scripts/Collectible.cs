d6f752fa008b939638ad252f9a673aeb0f1e3e47
Marcos Mundina Fabregat <discor2008125@gmail.com>
2025-11-08 17:29:15 +0100

    Improve collectible behavior and player interaction
    - Added particles and animation hiding logic to `Collectible.cs` during player interaction.
    - Adjusted player scale and capsule height in `player.tscn`.
    - Renamed `Background Layers` to `TilemapLayers` and updated parent hierarchy in `level1.tscn`.

--- Scripts/Collectible.cs
+++ Scripts/Collectible.cs
@@ -3,8 +3,18 @@ using System;
 
 public partial class Collectible : Area2D
 {
+    private CpuParticles2D particles;
+    private AnimatedSprite2D sprite;
+    
+    public override void _Ready()
+    {
+        particles = GetNode<CpuParticles2D>("CPUParticles2D");
+        sprite = GetNode<AnimatedSprite2D>("AnimatedSprite2D");
+    }
+    
     private void _on_body_entered(CharacterBody2D body)
     {
-        QueueFree();
+        particles.Emitting = true;
+        sprite.Hide();
     }
 }
