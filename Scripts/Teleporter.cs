5987b809265e3410cbe32b7ec83732e87f20332a
Marcos Mundina Fabregat <discor2008125@gmail.com>
2025-11-15 02:13:17 +0100

    Refactored teleporter functionality and updated resource configurations:
    - Removed `GPUParticles2D` from teleporter input area.
    - Adjusted symmetry settings in `Platform.tres` tileset.
    - Updated `Effectors.tres` collision points for improved accuracy.
    - Converted `ExitPosition` to `Node2D` and updated position handling in `Teleporter.cs`.
    - Adjusted parallax scrolling speed in `level1.tscn`.

--- Scripts/Teleporter.cs
+++ Scripts/Teleporter.cs
@@ -1,6 +1,7 @@
 using Godot;
 using System;
 
+[Tool]
 public partial class Teleporter : Node2D
 {
     private Area2D inputArea;
@@ -9,19 +10,20 @@ public partial class Teleporter : Node2D
     private AnimatedSprite2D inputSprite;
     private AnimatedSprite2D outputSprite;
 
-    [Export] public Vector2 ExitPosition;
+    [Export] public Node2D ExitPosition;
 
     private bool hasTeleported;
     
     public override void _Ready()
-    {
+    { 
+        
         inputArea = GetNode<Area2D>("InputArea");
         outputArea = GetNode<Area2D>("OutputArea");
         cooldownTimer = GetNode<Timer>("CooldownTimer");
         inputSprite = GetNode<AnimatedSprite2D>("InputArea/InputAnimatedSprite2D");
         outputSprite = GetNode<AnimatedSprite2D>("OutputArea/OutputAnimatedSprite2D");
         
-        outputArea.GlobalPosition = ExitPosition;
+        outputArea.GlobalPosition = ExitPosition.GlobalPosition;
     }
     
     private void _on_input_area_body_entered(Node2D body)
